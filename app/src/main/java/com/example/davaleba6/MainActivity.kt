3a729c4c4dac7413e2d79225ce9c8b5a9daddfb3
jemalisibashvili <jemali.sibashvili.1@btu.edu.ge>
2022-05-09 13:45:51 +0400

    davaleba6

---
+++ app/src/main/java/com/example/davaleba6/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.davaleba6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var bottomnavigationView: BottomNavigationView
+    private lateinit var navController: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bottomnavigationView = findViewById(R.id.bottomNavView)
+        navController = findNavController(R.id.navHostFragment)
+
+        setupActionBarWithNavController(navController, AppBarConfiguration(
+            setOf(R.id.homeFragment,
+            R.id.dashboardFragment,
+            R.id.notificationFragment)
+        ))
+        bottomnavigationView.setupWithNavController(navController)
+    }
+}
---
+++ app/src/main/java/com/example/davaleba6/fragments/DashboardFragment.kt
@@ -0,0 +1,20 @@
+package com.example.davaleba6.fragments
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.example.davaleba6.R
+
+class DashboardFragment: Fragment(R.layout.fragment_dashboard){
+    private lateinit var textView: TextView
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        textView = view.findViewById(R.id.textView)
+      val amount = DashboardFragmentArgs.fromBundle(requireArguments()).amount
+
+        textView.text = amount.toString()
+
+    }
+}
